db4a6d3162db46d1577b306574ede0dbbba7aed4
RangaCoogngne <drp.coogngne@gmail.com>
2018-03-12 16:44:13 +0530

    Initial Commit

---
+++ vidly/vidly/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { id = 1, Name = "Shaaaa" },
+                new Customer { id = 2, Name = "Waooow" }
+            };
+        }
+
+    }
+}
---
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+using vidly.ViewModels;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1" },
+                new Customer {Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel); 
+        }
+
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { id = 1, Name = "Shrek" },
+                new Movie { id = 2, Name = "Wall-e" }
+            };
+        }
+
+       
+    }
+}
